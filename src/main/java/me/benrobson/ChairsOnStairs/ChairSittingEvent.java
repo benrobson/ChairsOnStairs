aa032f697f5ef8429aac81cba520c99fa3262191
benrobson <benrobson76@gmail.com>
2021-02-17 11:13:26 +1100

    Players can now sit in chairs!

--- src/main/java/me/benrobson/ChairsOnStairs/ChairSittingEvent.java
+++ src/main/java/me/benrobson/ChairsOnStairs/ChairSittingEvent.java
@@ -1,7 +1,11 @@
 package me.benrobson.ChairsOnStairs;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,8 +23,11 @@ public class ChairSittingEvent implements Listener {
     @EventHandler
     public void onPlayerStairInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
+        Location PlayerLocation = event.getPlayer().getLocation();
         Block block = event.getClickedBlock();
         String BlockMaterial = block.getType().name();
+        Location BlockLocation = block.getLocation();
+        int ChairSittingRange = 0;
 
         if (block != null) {
             if (event.getAction() == Action.RIGHT_CLICK_BLOCK && !player.isInsideVehicle() && player.getInventory().getItemInMainHand().getType() == Material.AIR) {
@@ -29,11 +36,18 @@ public class ChairSittingEvent implements Listener {
                 while (ChairBlocks.hasNext()) {
                     String ChairBlock = (String)ChairBlocks.next();
 
-                    player.sendMessage(BlockMaterial);
-
                     if (BlockMaterial.equalsIgnoreCase(ChairBlock)) {
-                        player.sendMessage("This is a valid chair.");
+                        if (ChairSittingRange > 0 && PlayerLocation.distance(BlockLocation) - 1.0D > (double)ChairSittingRange) return; // Check if the player is next to the block.
+
+                        World world = player.getWorld();
+
+                        Entity ChairEntity = world.spawnEntity(player.getLocation(), EntityType.ARROW);
+                        ChairEntity.teleport(BlockLocation.add(0.5D, 0.2D, 0.5D));
+                        ChairEntity.setPassenger(player);
+                        event.setCancelled(true);
+                        return;
                     }
+
                 }
 
             }
